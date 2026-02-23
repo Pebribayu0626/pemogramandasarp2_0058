51a816141957b402574d734620f31100cdea4079
Pebribayu0626 <PebriBayuSatriansyah@gmail.com>
2026-02-23 21:06:53 +0700

    first commit

---
+++ p1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sks;
+
+int main(){
+    
+    int umur;
+    umur = 20;
+
+    cout << "Halo Mas Abra" << endl;
+    cout << 10 << endl;
+    cout << umur;
+    
+}
---
+++ p2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int p, l, luas;
+
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+
+    luas = p * l;
+
+    cout << "Hasilnya : " << luas;
+}
+   
